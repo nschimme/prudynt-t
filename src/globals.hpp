32bfb2e4b2a0aabc391494ff8eea339a76f1efec
Nils Schimmelmann <nschimme@gmail.com>
2025-09-16 15:23:21 +0000

    switch to monotonic clock

--- src/globals.hpp
+++ src/globals.hpp
@@ -29,10 +29,7 @@ struct AudioFrame
 struct H264NALUnit
 {
 	std::vector<uint8_t> data;
-    /* timestamp fix, can be removed if solved
 	struct timeval time;
-	int64_t imp_ts;
-    */
 };
 
 struct BackchannelFrame
